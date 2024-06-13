19c92c4d6e71c135aa24c155d52700eae71c207c
Dimitar Pashev <dim.pashev@gmail.com>
2024-06-13 15:48:25 +0300

    add Bootstrap, templates

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
 def project_list(request):
-    return HttpResponse("<h1>Hello, world. You're at the projects index.</h1>")
+    return render(request, "projects/index.html")
